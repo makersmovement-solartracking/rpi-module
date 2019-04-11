8c82c10c110f3b867a5224348f91d818c6042294
VRafael07 <vrsilveira07@gmail.com>
2019-04-11 11:00:21 -0300

    Finished mock

--- gpio_connector.py
+++ gpio_connector.py
@@ -2,8 +2,17 @@ import os
 if os.getenv("EXEC_MODE", "LOCAL") == "PROD":
     import RPi.GPIO as GPIO
     __LOCAL_MODE__ = False
+    OUT = GPIO.OUT
+    LOW = GPIO.LOW
+    HIGH = GPIO.HIGH
+    BCM = GPIO.BMC
 else:
     __LOCAL_MODE__ = True
+    # Variables declaration
+    OUT = "output"
+    LOW = "low"
+    HIGH = "high"
+    BCM = "bcm"
 
 
 def setup(pin_number, pin_type):
@@ -20,8 +29,28 @@ def output(pin_number, pin_voltage):
         GPIO.output(pin_number, pin_voltage)
 
 
-def pwm(pin_number, frequency):
+def PWM(pin_number, frequency):
     if __LOCAL_MODE__:
         print("Set pin {} to frequency {}".format(pin_number, frequency))
+        return MockPWM
     else:
         GPIO.PWM(pin_number, frequency)
+
+
+def setmode(operation):
+    if __LOCAL_MODE__:
+        print("Set board operation mode to {}".format(operation))
+    else:
+        GPIO.setmode(operation)
+
+
+class MockPWM():
+
+    def __init__(self):
+        pass
+
+    def start():
+        print("PWM start")
+
+    def ChangeDutyCycle(level):
+        print("Changed to {}".format(level))

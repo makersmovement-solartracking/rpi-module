962ef8c5f2b7a6183a697ac38fbe027ec98b6858
vitorrafael <vrsilveira07@gmail.com>
2019-04-21 10:57:19 -0700

    Fix some I2C bugs

--- i2c_connector.py
+++ i2c_connector.py
@@ -1,18 +1,18 @@
 from smbus2 import SMBus
 
-class I2C(SMBus):
+class I2C:
     """ Represents the I2C connection and all its
     functions necessary to create the communication
     between the Arduino and the Raspberry Pi. """
 
     def __init__(self, address):
-        super.__init__(1)
+        self.connector = SMBus(1)
         self.SLAVE_ADDRESS = address
 
 
     def get_arduino_data(self):
         """ Fetchs the Arduino data transfered through I2C. """
-        data  = read_i2c_block_data(address, 1)
+        data  = self.connector.read_i2c_block_data(self.SLAVE_ADDRESS, 1, 2)
         return data
 
     def convert_byte_to_integer(self, data):

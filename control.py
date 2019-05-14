b1f624b9873097cc10dae59453640e660166a9e8
vitorrafael <vrsilveira07@gmail.com>
2019-05-14 10:30:18 -0300

    Controls the tracking process

--- control.py
+++ control.py
@@ -1,8 +1,17 @@
 from controller import L298N
+from time import sleep
+import i2c_connector as i2c
+import datetime as dt
 
-motor = L298N()
-movements = ["low", "forward", "backward", "stop",
-             "this will produce an error"]
+i2c_slave = i2c.I2C(0x8, 4)
+i = 0
 
-for move in movements:
-    print(motor.move(move))
+while True:
+    try:
+        brute_data = i2c_slave.get_arduino_data()   
+    except ( IOError, OSError) as e:
+        continue
+    ldr_values = i2c.convert_byte_to_integer(brute_data)
+    date = dt.datetime.now()
+    print("{}:{}:{},{},{},{},{}".format(date.hour, date.minute, date.second, ldr_values[0], ldr_values[1], ldr_values[2], ldr_values[3]))
+    sleep(1)

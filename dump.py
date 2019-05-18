d34ee598ffd007396a83503b0ac1848047e3e3e6
vitorrafael <vrsilveira7@gmail.com>
2019-05-18 08:50:06 -0300

    Appends the data to a CSV file

--- dump.py
+++ dump.py
@@ -2,6 +2,7 @@ from controller import L298N
 from time import sleep
 import i2c_connector as i2c
 import datetime as dt
+from csv import writer
 
 i2c_slave = i2c.I2C(0x8, 4)
 i = 0
@@ -13,5 +14,10 @@ while True:
         continue
     ldr_values = i2c.convert_byte_to_integer(brute_data)
     date = dt.datetime.now()
-    print("{}:{}:{},{},{},{},{}".format(date.hour, date.minute, date.second, ldr_values[0], ldr_values[1], ldr_values[2], ldr_values[3]))
+    time_now = "{}:{}:{}".format(date.hour, date.minute, date.second)
+    msg = "{}, {}, {}, {}, {}".format(time_now, ldr_values[0], ldr_values[1], ldr_values[2], ldr_values[3])
+    values = [time_now,  ldr_values[0], ldr_values[1], ldr_values[2], ldr_values[3]]
+    with open("out.csv", "a") as file:
+        writer = writer()
+        writer.writerow(values)
     sleep(1)

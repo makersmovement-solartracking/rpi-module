79a482eee9f32133f3a9b677d955a53adf35abaa
vitorrafael <vrsilveira07@gmail.com>
2019-08-11 16:06:37 -0300

    Adds command-line arguments to output to a CSV file

--- dump.py
+++ dump.py
@@ -1,9 +1,10 @@
-""" Gets the value o n LDRs and append it to a csv file. """
+""" Gets the value of n LDRs and append it to a csv file. """
 
 from time import sleep
 from csv import writer
 from datetime import datetime
-from i2c_connector import I2C
+from i2c_connector import I2C, EmptyLDRListException, OddLDRListException, UnvalidLDRListValuesException
+import sys
 
 
 ADDRESS = 0X8
@@ -13,7 +14,7 @@ i2c_slave = I2C(ADDRESS, LDR_COUNT)
 
 def write_to_csv_file(data, filename="out.csv"):
     """ Appends the data to a CSV file. """
-    with open(filename) as csv_file:
+    with open(filename, 'wb') as csv_file:
         csv_writer = writer(csv_file)
         csv_writer.writerow(data)
 
@@ -28,25 +29,23 @@ def get_ldr_values_by_time_string(data):
     return ldr_message
 
 
-def get_csv_data_list(data):
-    """ Creates the list that will be used by the CSV
-    writer to append the data to the CSV file. """
-    time = datetime.now()
-    ldr_message = "{}:{}:{}".format(time.hour, time.minute, time.second)
-    csv_data = [ldr_message]
-    for value in data:
-        csv_data.append(value)
-    return csv_data
-
-
 while True:
     try:
-        LDR_VALUES = i2c_slave.get_ldr_values()
+        sleep(1)
+        ldr_values = i2c_slave.get_ldr_values()
+
     except (IOError, OSError):
         continue
-    CSV_DATA = get_csv_data_list(LDR_VALUES)  # Creates the CSV list
-    write_to_csv_file(CSV_DATA)  # Appends the data to the CSV file
-    # Get the LDR values string
-    LDR_VALUES_BY_TIME = get_ldr_values_by_time_string(LDR_VALUES)
-    print(LDR_VALUES_BY_TIME)  # Prints the LDR values
-    sleep(1)
+
+    except (OddLDRListException, EmptyLDRListException, UnvalidLDRListValuesException) as e:
+        print(str(e))
+        continue
+
+    ldr_values_by_time = get_ldr_values_by_time_string(ldr_values)
+    print(ldr_values_by_time)
+
+    if len(sys.argv) > 1 and str(sys.argv[1]) is '-c':
+        ldr_values_by_time.replace(' ', ',')
+        write_to_csv_file(ldr_values_by_time)
+
+

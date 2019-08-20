9460021ada9253d78fb94bd4575be1e49f0b62d9
vitorrafael <vrsilveira07@gmail.com>
2019-08-20 19:22:04 -0300

    i2c_connector.py: Transfers the responsibility of raising the invalid ldr list exception

--- i2c_connector.py
+++ i2c_connector.py
@@ -20,6 +20,8 @@ class I2C:
         """ Gets the ldr values and validates it. """
         brute_data = self.get_arduino_data()
         ldr_values = convert_byte_to_integer(brute_data)
+        if not is_valid_ldr_list(ldr_values):
+            raise InvalidLDRListException
         if not is_valid_ldr_data(ldr_values):
             raise InvalidLDRListValuesException
         return ldr_values

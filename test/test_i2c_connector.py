16d9c47d6fe7096eb39a70b9f4133d5b836430a7
vitorrafael <vrsilveira07@gmail.com>
2019-08-20 19:30:10 -0300

    test_i2c_connector.py: Refactors the code to be compatible with the new exceptions

--- test/test_i2c_connector.py
+++ test/test_i2c_connector.py
@@ -12,38 +12,31 @@ def test_aggregate_bytes():
 def test_empty_ldr_list():
     """ Tests the answer if the ldr list is empty"""
     ldr_list = []
-    with pytest.raises(i2c.EmptyLDRListException) as excinfo:
-        i2c.check_ldr_list_length(ldr_list)
-    assert "LDR list is empty." in str(excinfo.value)
+    assert i2c.is_valid_ldr_list(ldr_list) is False
 
 
 def test_odd_ldr_list():
     """ Tests the answer if the ldr list is odd."""
     ldr_list = [255, 255, 255]
-    with pytest.raises(i2c.OddLDRListException) as excinfo:
-        i2c.check_ldr_list_length(ldr_list)
-    assert "LDR list is odd." in str(excinfo.value)
-
+    assert i2c.is_valid_ldr_list(ldr_list) is False
 
 
 def test_even_ldr_list():
     """ Tests the software behavior when the ldr list is
     even. """
     ldr_list = [255, 255, 255, 255]
-    assert i2c.check_ldr_list_length(ldr_list) is None
+    assert i2c.is_valid_ldr_list(ldr_list) is True
 
 
 def test_invalid_ldr_values_list():
     """ Tests the validation of an invalid ldr values
     list. """
     ldr_list = [300, 487, 908, 65535]
-    with pytest.raises(i2c.UnvalidLDRListValuesException) as excinfo:
-        i2c.validates_ldr_data(ldr_list)
-    assert "Transfered invalid values." in str(excinfo.value)
+    assert i2c.is_valid_ldr_data(ldr_list) is False
 
 
 def test_valid_ldr_values_list():
     """ Tests the validation of a valid ldr values
     list. """
     ldr_list = [300, 487, 908, 920]
-    assert i2c.validates_ldr_data(ldr_list) is True
+    assert i2c.is_valid_ldr_data(ldr_list) is True
